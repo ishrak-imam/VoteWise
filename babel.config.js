959fad0fa2a040130a14c8c9b5d905fb7edb5753
Ishrak Imam <ishrak@litentry.com>
2022-08-12 12:16:50 +0200

    Fixing module resolution

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@polkadotApi': './src/polkadotApi',
+          '@context': './src/context',
+          '@service': './src/service',
+          '@hooks': './src/hooks',
+          '@atoms': './src/atoms',
+        },
+      },
+    ],
+  ]
 };
